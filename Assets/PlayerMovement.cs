b098077b81ee619d3c0cc25658eb7d77c080140c
huyca0403 <huycachuoi@gmail.com>
2025-02-12 22:02:53 +0700

    them double jump va do cao cua cu bat nhay dua theo thoi gian giu phim cach

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,11 +11,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 2f;
     [SerializeField] private float jumpForce = 10f;
     [SerializeField] private LayerMask collisionMask;
-
+    [SerializeField] private int maxJump = 2;
+    [SerializeField] private float _yValJumpReleaseMod = 2f;
+    private int _jumpLeft;
     // Start is called before the first frame update
     void Start()
     {
-        _rigibody = GetComponent<Rigidbody2D>();    
+        _rigibody = GetComponent<Rigidbody2D>();
+        _jumpLeft = maxJump;
     }
 
     // Update is called once per frame
@@ -23,12 +26,23 @@ public class PlayerMovement : MonoBehaviour
     {
         var inputX = Input.GetAxisRaw("Horizontal");
         var jumpInput = Input.GetButtonDown("Jump");
-
+        var jumpInputReleased = Input.GetButtonUp("Jump");
         _rigibody.velocity = new Vector2(inputX * speed, _rigibody.velocity.y);
 
-        if(jumpInput && IsGround())
+        if(IsGround() && _rigibody.velocity .y <= 0)
+        {
+            _jumpLeft = maxJump;
+        }
+
+        if(jumpInput && _jumpLeft > 0)
         {
             _rigibody.velocity = new Vector2(_rigibody.velocity.x, jumpForce);
+            _jumpLeft -= 1;
+        }
+
+        if (jumpInputReleased && _rigibody.velocity.y > 0 && _jumpLeft == 1)
+        {
+            _rigibody.velocity = new Vector2(_rigibody.velocity.x, _rigibody.velocity.y/_yValJumpReleaseMod);
         }
 
         if(inputX != 0)
